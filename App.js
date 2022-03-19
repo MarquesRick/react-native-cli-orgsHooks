79307a7e24fab1c920b9da368907ce7e2a244ca6
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-03-19 01:08:46 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>Main</Text>
+      </SafeAreaView>
+    </>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,4 @@
+criar projeto: npx react-native init orgsHook
+rodar no android: npx react-native run-android
+rodar no ios: npx react-native run-ios
+rodar o metro: npm start
