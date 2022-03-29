ef5cfc887e466e1d9e4ff8ff68c57b003bb19d9c
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-03-29 19:26:52 -0300

    correction src/routes

--- src/routes/AppRotas.js
+++ src/routes/AppRotas.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 export default function AppRotas() {
   return (
     <NavigationContainer>
