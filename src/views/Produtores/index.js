ceceabdf9e3595b9d2f0a08e7d5efe6096728ae0
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-03-30 19:52:34 -0300

    removing message after 3sec

--- src/views/Produtores/index.js
+++ src/views/Produtores/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { FlatList, Text, StyleSheet } from 'react-native';
 
@@ -10,16 +10,34 @@ import useTextos from '../../hooks/useTextos';
 export default function Produtores({ melhoresProdutores }) {
   const navigation = useNavigation();
   const route = useRoute();
+  const [exibeMensagem, setExibeMensagem] = useState(false);
   const nomeCompra = route.params?.compra.nome;
+  const timeStampCompra = route.params?.compra.timeStamp;
   const lista = useProdutores(melhoresProdutores);
   const { tituloProdutores, mensagemCompra } = useTextos();
   const mensagemCompleta = mensagemCompra?.replace('$NOME', nomeCompra);
+
+  useEffect(() => {
+    setExibeMensagem(!!nomeCompra);
+    let timeout;
+
+    if (nomeCompra) {
+      timeout = setTimeout(() => {
+        setExibeMensagem(false);
+      }, 3000);
+    }
+    //para limpar o timeout em caso de nao executar varios ao mesmo tempo
+    return () => clearTimeout(timeout);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timeStampCompra]);
 
   const TopoLista = () => {
     return (
       <>
         <Topo melhoresProdutores={melhoresProdutores} />
-        {!!nomeCompra && <Text style={estilos.compra}>{mensagemCompleta}</Text>}
+        {exibeMensagem && (
+          <Text style={estilos.compra}>{mensagemCompleta}</Text>
+        )}
         <Text style={estilos.titulo}>{tituloProdutores}</Text>
       </>
     );
